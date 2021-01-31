f990bfe6082e01edfd2b17657d06bc79b8a2a0ca
sameer13899 <sameersoni389@gmail.com>
2021-02-01 00:22:17 +0530

    fast power using recursion

--- recursion/Test1.java
+++ recursion/Test1.java
@@ -7,11 +7,20 @@ public class Test1 {
         return x * xPowerN(x, n - 1);
     }
 
+    public static int xPowerNFast(int x, int n) {
+        if (n == 0)
+            return 1;
+        int small = xPowerNFast(x, n / 2);
+        if (n % 2 == 0)
+            return small * small;
+        return x * small * small;
+    }
+
     public static void main(String[] args) {
         // x power n
         Scanner in = new Scanner(System.in);
         int x = in.nextInt();
         int n = in.nextInt();
-        System.out.print(xPowerN(x, n));
+        System.out.print(xPowerNFast(x, n));
     }
 }
