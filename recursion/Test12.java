048e9924a595aac20dc8dfc82dab61303f99b415
sameer13899 <sameersoni389@gmail.com>
2021-02-04 01:47:35 +0530

    find all the indexes of the given number in an array updated

--- recursion/Test12.java
+++ recursion/Test12.java
@@ -1,25 +1,16 @@
 import java.util.Scanner;
 
 public class Test12 {
-    public static void findIndexes(int[] a, int[] b, int x, int i, int j) {
-        if (i == a.length)
-            return;
+    public static int findIndexes(int[] a, int[] b, int x, int i, int j) {
+        if (i == a.length) {
+            return j;
+        }
         if (a[i] == x) {
             b[j] = i;
             j++;
         }
-        findIndexes(a, b, x, i + 1, j);
-    }
-
-    public static void findIndexes(int[] a, int x) {
-        int[] b = new int[a.length];
-        findIndexes(a, b, x, 0, 0);
-        for (int i = 0; i < a.length; i++) {
-            if (b[i] == 0) {
-                break;
-            }
-            System.out.print(b[i] + " ");
-        }
+        int smallAns = findIndexes(a, b, x, i + 1, j);
+        return smallAns;
     }
 
     public static void main(String[] args) {
@@ -30,7 +21,16 @@ public class Test12 {
             a[i] = in.nextInt();
         }
         int x = in.nextInt();
-        findIndexes(a, x);
+        int[] b = new int[n];
+        int l = findIndexes(a, b, x, 0, 0);
+        if (l == 0) {
+
+            System.out.print(0 + " ");
+        } else {
+            for (int i = 0; i < l; i++) {
+                System.out.print(b[i] + " ");
+            }
+        }
     }
 
 }
